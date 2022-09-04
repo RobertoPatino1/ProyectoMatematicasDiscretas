9ef55ffb75e55931f787d463be21eb64a157c3f7
RobertoPatino1 <rapatino@espol.edu.ec>
2022-09-04 02:09:51 -0500

    Creacion del segundo nivel

--- proyectoMD/src/main/java/com/mycompany/proyectomd/Nivel2Controller.java
+++ proyectoMD/src/main/java/com/mycompany/proyectomd/Nivel2Controller.java
@@ -5,97 +5,51 @@
  */
 package com.mycompany.proyectomd;
 
-import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 /**
+ * FXML Controller class
  *
- * @author josel
+ * @author Det-Pc
  */
-public class Nivel2Controller {
-    private void comprobarResultados(ActionEvent event) {
-        
-        
-        
-     
-    }
-    
-    private boolean comprobarSpiderman(String entrada){
-        String[] respuestas =entrada.split(",");
-        boolean resultado=false;
-        boolean r1=false;
-        boolean r2=false;
-        for(String respuesta:respuestas){
-            if(respuesta.trim().toUpperCase()=="SUPERMAN"){
-                r1=true;
-            }
-            if(respuesta.trim().toUpperCase()=="THOR"){
-                r2=true;
-            }
-        }  
-        
-        resultado=r1&&r2;
-        return resultado;
-    }
-    
-    private boolean comprobarSuperman(String entrada){
-        String[] respuestas =entrada.split(",");
-        boolean resultado=false;
-        boolean r1=false;
-        boolean r2=false;
-        boolean r3=false;
-        for(String respuesta:respuestas){
-            if(respuesta.trim().toUpperCase()=="SPIDERMAN" || respuesta.trim().toUpperCase()=="SPIDER MAN" || respuesta.trim().toUpperCase()=="SPIDER-MAN"){
-                r1=true;
-            }
-            if(respuesta.trim().toUpperCase()=="IRONMAN" || respuesta.trim().toUpperCase()=="IRON MAN"){
-                r2=true;
-            }
-            if(respuesta.trim().toUpperCase()=="THOR"){
-                r3=true;
-            }
-        }  
-        
-        resultado=r1&&r2&&r3;
-        return resultado;
-    }
-    
-    private boolean comprobarThor(String entrada){
-        String[] respuestas =entrada.split(",");
-        boolean resultado=false;
-        boolean r1=false;
-        boolean r2=false;
-        for(String respuesta:respuestas){
-            if(respuesta.trim().toUpperCase()=="SUPERMAN"){
-                r1=true;
-            }
-            if(respuesta.trim().toUpperCase()=="SPIDERMAN" || respuesta.trim().toUpperCase()=="SPIDER MAN" || respuesta.trim().toUpperCase()=="SPIDER-MAN"){
-                r2=true;
-            }
-        }  
-        
-        resultado=r1&&r2;
-        return resultado;
-    }
-    
-    private boolean comprobarIronman(String entrada){
-        String[] respuestas =entrada.split(",");
-        boolean resultado=false;
-        boolean r1=false;
-        boolean r2=false;
-        for(String respuesta:respuestas){
-            if(respuesta.trim().toUpperCase()=="SUPERMAN"){
-                r1=true;
-            }
-            if(respuesta.trim().toUpperCase()=="SPIDERMAN" || respuesta.trim().toUpperCase()=="SPIDER MAN" || respuesta.trim().toUpperCase()=="SPIDER-MAN"){
-                r2=true;
-            }
-        }  
-        
-        resultado=r1&&r2;
-        return resultado;
-    }
-    
-    
-    
+public class Nivel2Controller implements Initializable {
+
+
+    @FXML
+    private ImageView imgvSuperman;
+    @FXML
+    private TextField txtSuperman1;
+    @FXML
+    private ImageView imgvSpiderman;
+    @FXML
+    private TextField txtSpiderman1;
+    @FXML
+    private ImageView imgvThor;
+    @FXML
+    private TextField txtThor1;
+    @FXML
+    private ImageView imgvIronman;
+    @FXML
+    private TextField txtIronman1;
+    @FXML
+    private ImageView imgvSuperman2;
+    @FXML
+    private ImageView imgvSpiderman2;
+    @FXML
+    private ImageView imgvThor2;
+    @FXML
+    private ImageView imgvIronman2;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
     
 }
